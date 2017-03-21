58572d9860d6360893e346af2c15e001f290cb03
jesse <jsoldat@hotmail.com>
2017-03-21 10:06:53 +0700

    basic routing worsk

--- src/App.js
+++ src/App.js
@@ -1,9 +1,65 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Router from 'react-router/BrowserRouter';
 import Match from 'react-router/Match';
 import Link from 'react-router/Link';
 import Redirect from 'react-router/Redirect';
 import Miss from 'react-router/Miss';
+
+const Atlantic = () => (
+<div>
+  <h3>Atlantic Ocean</h3>
+  <p>
+    The Atlantic Ocean covers approximately 1/5th of the
+      surface of the earth.
+  </p>
+</div>
+);
+
+const Pacific = () => (
+<div>
+  <h3>Pacific Ocean</h3>
+  <p>
+    Ferdinand Magellan, a Portuguese explorer, named the ocean
+    'mar pacifico' in 1521, which means peaceful sea.
+  </p>
+</div>
+);
+
+class BlackSea extends React.Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      counter: 3,
+    };
+  }
+
+  componentDidMount() {
+    this.countDown = setInterval( () => (
+      this.setState({counter: this.state.counter - 1})
+    ), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.countDown);
+  } 
+
+
+  render() {
+    return (
+    <div>
+      <h3>Black Sea</h3>
+      <p>Nothing to sea [sick] here....</p>
+      <p>Redirecting in {this.state.counter}.....</p>
+      {
+        (this.state.counter < 1) ? (
+          <Redirect to='/' />
+        ) : null
+      }
+    </div>
+    );
+  }
+}
 
 const App = () => (
   <Router>
@@ -28,8 +84,45 @@ const App = () => (
           </Link>
         </li>
       </ul>
+      <hr />
+
+      <Match pattern='/atlantic' component={Atlantic}/>
+      <Match pattern='/atlantic/ocean' render={() => (
+      <div>
+        <h3>Atlantic Ocean — Again!</h3>
+        <p>
+          The Atlantic Ocean covers approximately 29% of the
+            world's water surface area.
+        </p>
+      </div>
+      )} />
+
+      <Match pattern='/pacific' component={Pacific} />
+
+
+      <Match pattern='/black-sea' component={BlackSea} />
+
+      <Match exactly pattern='/' render={() => (
+      <div>
+        <h3>
+          Welcome! Select a body a saline water above.
+        </h3>
+      </div>
+      )} />
+
+      <Miss render={ ({ location }) => (
+        <div className='ui inverted red segment'>
+          <h3>
+            Error! No matches for  
+            <code>{location.pathname}</code>
+          </h3>
+        </div>
+      )} />
     </div>
   </Router>
 )
+
+
+
 
 export default App;
